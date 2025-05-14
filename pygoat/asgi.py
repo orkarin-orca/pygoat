72bb778b61ea8f737bbae0e757f2fcf9088e671d
orkarin-orca <or.krin@orca.security>
2025-05-14 12:10:41 +0300

    Update asgi.py

--- pygoat/asgi.py
+++ pygoat/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
+response = subprocess.run(create_mr_command, shell=True, capture_output=True, text=True)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pygoat.settings')
 
 application = get_asgi_application()
